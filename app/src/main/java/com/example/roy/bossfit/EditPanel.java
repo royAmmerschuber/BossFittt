60ce0137bb1dfe213f055e11317117f5bfbc6771
royAmmerschuber <roy@ammerschuber.de>
2018-02-27 11:56:19 +0100

    V1.2 start

---
+++ app/src/main/java/com/example/roy/bossfit/EditPanel.java
@@ -0,0 +1,13 @@
+package com.example.roy.bossfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_panel);
+    }
+}
